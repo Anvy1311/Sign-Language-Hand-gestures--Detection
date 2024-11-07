0eade82ef809bacaca355c3d7efda9ec8bb79fe5
Anvy1311 <anvymaheshwari@gmail.com>
2024-11-07 20:20:46 +0530

    Final push

--- gestures.py
+++ gestures.py
@@ -50,3 +50,27 @@ while True:
                 lmy = int(lm.y * y)
 
                 landmarks.append([lmx, lmy])
+
+            # Drawing landmarks on frames
+            mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
+
+            # Predict gesture
+            prediction = model.predict([landmarks])
+            # print(prediction)
+            classID = np.argmax(prediction)
+            className = classNames[classID]
+
+    # show the prediction on the frame
+    cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (0,0,255), 2, cv2.LINE_AA)
+
+    # Show the final output
+    cv2.imshow("Output", frame) 
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# release the webcam and destroy all active windows
+cap.release()
+
+cv2.destroyAllWindows()
